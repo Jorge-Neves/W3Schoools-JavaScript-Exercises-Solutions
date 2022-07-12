af0774428e129e541c17545fa68c211f1dc265cf
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-07-12 23:59:57 +0100

    Add files via upload

---
+++ 01-JS-Operators/Exercise-03.js
@@ -0,0 +1,13 @@
+/*
+    
+    ###################
+    #   Exercise : 03 #
+    ###################
+
+    Alert the remainder when 15 is divided by 9.
+
+    console.log(15  9);
+
+*/
+
+console.log(15 % 9);
