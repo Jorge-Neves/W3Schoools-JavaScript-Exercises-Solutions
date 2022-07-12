ac82300f20a467064d4f2725227aa3d44d158f35
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-07-12 23:55:38 +0100

    Add files via upload

--- 01-JS-Operators/Exercise-01.js
+++ 01-JS-Operators/Exercise-01.js
@@ -1,1 +1,13 @@
+/*
+    
+    ###################
+    #   Exercise : 01 #
+    ###################
+
+    Multiply 10 with 5, and alert the result:
+
+    console.log(10  5);
+
+*/
+
 console.log(10 * 5);
