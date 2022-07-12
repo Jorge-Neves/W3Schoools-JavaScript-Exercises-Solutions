fad6463955261df3727730b7579a1467386f6a49
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-07-12 23:57:30 +0100

    JS Operators Exercise 2

---
+++ 01-JS-Operators/Exercise-02.js
@@ -0,0 +1,13 @@
+/*
+    
+    ###################
+    #   Exercise : 02 #
+    ###################
+    
+    Divide 10 by 2, and alert the result:
+
+    console.log(10 2);
+
+*/
+
+console.log(10 / 2);
